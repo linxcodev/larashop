931bccf3ec9c9e45e3d6922d097902c01ccc4e3c
Ahmad Fajrul Falah <fajrul452@gmail.com>
2019-02-04 19:33:41 +0700

    layout dan styling

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,7 @@
-@extends('layouts.app')
+@extends("layouts.global")
 
+@section("title") Home @endsection
+@section("pageTitle") Home @endsection
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
--- routes/web.php
+++ routes/web.php
@@ -12,9 +12,13 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('login');
 });
 
 Auth::routes();
+
+Route::match(["GET", "POST"], "/register", function(){
+  return redirect("/login");
+});
 
 Route::get('/home', 'HomeController@index')->name('home');
